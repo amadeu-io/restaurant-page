ac51a439682773dfe09ba50c6992a6d963e34ab1
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-08 18:54:01 -0500

    Build basic landing page with HTML & CSS
    The svg icons needed are also imported on the JS and ready to be used
    later when all the HTML code is build in JS

---
+++ README.md
@@ -0,0 +1,23 @@
+# Restaurant Page 🍔
+
+<p align="center">
+  <img src="" width="650px" alt="screenshot">
+</p>
+<h2 align="center">
+  <a href="https://nightrunner4.github.io/">👉 Demo</a>
+</h2>
+
+### About 📖
+
+Third project of the JavaScript chapter in [The Odin Project](https://www.theodinproject.com). An example of building a page with webpack.
+
+### Resources Used 📚
+
+- [Google Fonts](https://fonts.google.com) (Icons & Font)
+
+### Built With 🛠️
+
+- Webpack
+- JS
+- HTML
+- CSS
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import './style.css';
+import "./style.css";
+import beverages from "./icons/beverages.svg";
+import breakfast from "./icons/breakfast.svg";
+import appetizers from "./icons/appetizers.svg";
+import lunch from "./icons/lunch.svg";
+
+let icons = [beverages, breakfast, appetizers, lunch];
+
+const content = document.getElementById("content");
+
+const h1 = document.createElement("h1");
+h1.textContent = "Bambuda Lodge Menu";
+content.appendChild(h1);
+
+icons.forEach((icon) => {
+  let imgIcon = new Image();
+  imgIcon.src = icon;
+  content.appendChild(imgIcon);
+});
