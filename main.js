9b8164c944c3869ed07f9e5961c930a851354775
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-08 18:54:01 -0500

    Build basic landing page with HTML & CSS
    The svg icons needed are also imported on the JS and ready to be used
    later when all the HTML code is build in JS

--- main.js
+++ main.js
@@ -116,7 +116,47 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _icons_beverages_svg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./icons/beverages.svg */ \"./src/icons/beverages.svg\");\n/* harmony import */ var _icons_breakfast_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./icons/breakfast.svg */ \"./src/icons/breakfast.svg\");\n/* harmony import */ var _icons_appetizers_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./icons/appetizers.svg */ \"./src/icons/appetizers.svg\");\n/* harmony import */ var _icons_lunch_svg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./icons/lunch.svg */ \"./src/icons/lunch.svg\");\n\n\n\n\n\n\nlet icons = [_icons_beverages_svg__WEBPACK_IMPORTED_MODULE_1__, _icons_breakfast_svg__WEBPACK_IMPORTED_MODULE_2__, _icons_appetizers_svg__WEBPACK_IMPORTED_MODULE_3__, _icons_lunch_svg__WEBPACK_IMPORTED_MODULE_4__];\n\nconst content = document.getElementById(\"content\");\n\nconst h1 = document.createElement(\"h1\");\nh1.textContent = \"Bambuda Lodge Menu\";\ncontent.appendChild(h1);\n\nicons.forEach((icon) => {\n  let imgIcon = new Image();\n  imgIcon.src = icon;\n  content.appendChild(imgIcon);\n});\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/icons/appetizers.svg":
+/*!**********************************!*\
+  !*** ./src/icons/appetizers.svg ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"b80b4f4d5ea6699a4b66.svg\";\n\n//# sourceURL=webpack://restaurant-page/./src/icons/appetizers.svg?");
+
+/***/ }),
+
+/***/ "./src/icons/beverages.svg":
+/*!*********************************!*\
+  !*** ./src/icons/beverages.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"f21f122dfe10d79e896c.svg\";\n\n//# sourceURL=webpack://restaurant-page/./src/icons/beverages.svg?");
+
+/***/ }),
+
+/***/ "./src/icons/breakfast.svg":
+/*!*********************************!*\
+  !*** ./src/icons/breakfast.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"061a267e1da187bafafd.svg\";\n\n//# sourceURL=webpack://restaurant-page/./src/icons/breakfast.svg?");
+
+/***/ }),
+
+/***/ "./src/icons/lunch.svg":
+/*!*****************************!*\
+  !*** ./src/icons/lunch.svg ***!
+  \*****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"95ab3d36dff5e853bfbc.svg\";\n\n//# sourceURL=webpack://restaurant-page/./src/icons/lunch.svg?");
 
 /***/ })
 
@@ -171,6 +211,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -185,6 +237,26 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
