2069a6caa283bcd6322841592e52ffadf6353790
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-08 20:53:07 -0500

    Add landing page as a separate module
    The JS code that loads all the HTML code for the landing page
    has been put on a separate module named landingLoad, and it's been
    then called in the main 'index.js' file. Achieving the same result
    as before, but in a more organized way

--- src/index.js
+++ src/index.js
@@ -1,43 +1,7 @@
 import "./style.css";
-import beverages from "./icons/beverages.svg";
-import breakfast from "./icons/breakfast.svg";
-import appetizers from "./icons/appetizers.svg";
-import lunch from "./icons/lunch.svg";
+import { landingLoad } from './landingLoad.js';
 
-let icons = [beverages, breakfast, appetizers, lunch];
-let iconsString = [
-  "Beverages",
-  "Breakfast",
-  "Appetizers & Bites",
-  "Lunch & Dessert",
-];
+landingLoad();
 
-const content = document.getElementById("content");
 
-// create title
-const h1 = document.createElement("h1");
-h1.textContent = "Bambuda Lodge Menu";
-content.appendChild(h1);
 
-// create menu items
-for (let i = 0; i < 4; i++) {
-  // create container
-  const itemContainer = document.createElement("div");
-  itemContainer.classList = "item-container";
-
-  // create icon
-  let itemIcon = document.createElement("img");
-  itemIcon.src = icons[i];
-
-  // create name
-  let itemName = document.createElement("div");
-  itemName.classList = "item-name";
-  itemName.textContent = iconsString[i];
-
-  // add icon & name to container
-  itemContainer.appendChild(itemIcon);
-  itemContainer.appendChild(itemName);
-
-  // add container to content
-  content.appendChild(itemContainer);
-}
