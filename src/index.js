a259cc34fd314c5a606990f300c35ce0fb49b31b
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-10 18:30:38 -0500

    Create breakfast page HTML content & styles
    The HTML code is temporarily located in dist/index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import "./styles/reset.css";
 import "./styles/global.css";
+import "./styles/breakfast.css";
 
-import { landingLoad } from "./landingLoad.js";
-landingLoad();
-
-//import "./styles/breakfast.css";
+/* import { landingLoad } from "./landingLoad.js";
+landingLoad(); */
