ea48bdeed406fab95b7393a88723d20065e6a08c
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-15 17:17:13 -0500

    Add nav and appetizers content

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,19 +1,21 @@
 // loads the HTML and CSS of nav
 
-import '../css/nav.css';
+import "../css/reset.css";
+import "../css/global.css";
+import "../css/nav.css";
 
-const navLoad = function() {
+const navLoad = function () {
   const nav = document.querySelector(".nav");
 
   let navHTML = `
   <div class="nav-item" id="menu">Menu</div>
   <div class="nav-item" id="beverages">&#x1F379; Beverages</div>
   <div class="nav-item" id="breakfast">&#x1F373; Breakfast</div>
-  <div class="nav-item" id="appetizers">&#x1F32E; Appetizers &amp; Bites</div>
+  <div class="nav-item" id="appetizers"><a href="./appetizers.html">&#x1F32E; Appetizers &amp; Bites</a></div>
   <div class="nav-item" id="lunch">&#x1F35D; Lunch &amp; Dessert</div>
   `;
 
   nav.innerHTML = navHTML;
-}
+};
 
 export { navLoad };
